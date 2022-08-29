5cc9f5921df73606fafb1da33da6045988a7eac1
James Mitofsky <JamesMitofsky@gmail.com>
2022-08-29 18:23:03 +0200

    Delete unused code

--- src/components/Task.jsx
+++
@@ -1,9 +0,0 @@
-export default function Task({ id, title, description }) {
-  return (
-    <div key={id}>
-      <div>{id}</div>
-      <div>{title}</div>
-      <div>{description}</div>
-    </div>
-  );
-}
