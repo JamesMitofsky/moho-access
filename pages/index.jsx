1515ce323b22b0c741387cc77ae433c0f30df579
James Mitofsky <JamesMitofsky@gmail.com>
2022-09-01 18:18:43 +0200

    Change the casing convention for routes to be lower case

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,7 +23,7 @@ export default function App() {
     }
     // has state.js determined user is authorized to access QR codes?
     if (user.authorized) {
-      Router.push("/Access");
+      Router.push("/access");
     }
   }, [user]);
 
