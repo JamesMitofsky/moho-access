c535d353bc7b1e6c811699204102a1f9040b95bd
James Mitofsky <JamesMitofsky@gmail.com>
2022-09-03 14:01:11 +0200

    Big commit with various initializing changes to add global keys

---
+++ functions/code-access/getGlobalKeyCode.js
@@ -0,0 +1,9 @@
+import { getDoc, doc } from "firebase/firestore";
+import { db } from "../../services/firebase";
+import getCodeFromWeekdays from "../../functions/getCodeFromWeekdays";
+
+export default async function getGlobalKeyCode({
+  keyParam,
+  setCodeData,
+  setLoaded,
+}) {}
