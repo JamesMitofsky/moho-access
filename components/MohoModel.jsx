2d2aa80a215c5be2da45d783c8d0d11d85099ee7
James Mitofsky <JamesMitofsky@gmail.com>
2022-09-05 13:55:59 +0200

    Make button in model clickable

--- components/MohoModel.jsx
+++ components/MohoModel.jsx
@@ -1,6 +1,7 @@
 import Spline from "@splinetool/react-spline";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Box } from "@mui/material";
+import Router from "next/router";
 import Loading from "../components/Loading";
 
 export default function MohoModel() {
@@ -9,18 +10,30 @@ export default function MohoModel() {
     opacity: 0,
     transition: "all ease 4s",
   });
-  function onLoad() {
+  const loginRef = useRef(null);
+
+  function onMouseDown(e) {
+    if (e.target.name !== loginRef.current.name) return;
+
+    Router.push("/user-login");
+  }
+
+  function onLoad(spline) {
     setStyles((prev) => ({
       ...prev,
       opacity: 1,
-      transition: "all ease 4s",
+      transition: "opacity ease 1s",
     }));
     setLoaded(true);
+    loginRef.current = spline.findObjectById(
+      "a421548e-854c-4c3c-bb47-d54f0129cff2"
+    );
   }
   return (
     <Box sx={{ flex: 1 }} style={styles}>
-      {!loaded && <Loading loaded={loaded} />}
+      {/* {!loaded && <Loading loaded={loaded} />} */}
       <Spline
+        onMouseDown={onMouseDown}
         onLoad={onLoad}
         scene="https://prod.spline.design/iF8skT1diJd5tAsE/scene.splinecode"
       />
