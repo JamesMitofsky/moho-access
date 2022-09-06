8201d24bbe5718f6e64a0c15dda0dd4d9faecfce
James Mitofsky <JamesMitofsky@gmail.com>
2022-09-06 08:33:21 +0200

    Clarify copy for about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,19 +1,22 @@
 import MohoModel from "../components/MohoModel";
 import MarginProvider from "../components/layouts/MarginProvider";
-import { Typography } from "@mui/material";
+import { Typography, Link, Button } from "@mui/material";
+import WelcomeText from "../components/WelcomeText";
 
 export default function AboutPage() {
   return (
     <MarginProvider>
+      <WelcomeText />
+      <Link href="/code">Return to QR code</Link>
       <Typography variant="h1">Hi there, my name is James!</Typography>
-      <Typography variant="h2">
-        Programming is what I love, and I am looking for a full time job where I
-        can contribute to something bigger.
+      <Typography>
+        Powered by React, Next.js, Firebase Auth, and and Cloud Firestore, this
+        project was really interesting to work on. I've been doing freelance
+        development for 4 years, and now I'm looking for a full time position.
       </Typography>
-      <Typography variant="body1">
-        This project is powered by React, Next.js, Firebase Auth, and and Cloud
-        Firestore.
-      </Typography>
+      <Link target="_blank" href="https://github.com/JamesMitofsky">
+        <Button variant="contained">Visit my GitHub!</Button>
+      </Link>
       <MohoModel />
     </MarginProvider>
   );
