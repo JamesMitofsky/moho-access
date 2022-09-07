aea0fc3a2f5b82ac0d741c3811923052be220363
James Mitofsky <JamesMitofsky@gmail.com>
2022-09-07 17:31:04 +0200

    Prevent render of admin page button in the footer if the user is already on the admin page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,15 @@
 import { Box, Typography, Link, Button } from "@mui/material";
 import { useAppContext } from "../context/state";
+import { useRouter } from "next/router";
 
 const Footer = () => {
   const user = useAppContext();
   const isAdmin = user?.authorized;
+
+  const router = useRouter();
+  const pathName = router.pathname;
+  console.log(pathName);
+  const isAdminView = pathName.includes("admin");
   return (
     <Box
       component="footer"
@@ -14,7 +20,7 @@ const Footer = () => {
         padding: 2,
       }}
     >
-      {isAdmin && (
+      {isAdmin && !isAdminView && (
         <Button sx={{ mb: 2 }} variant="contained" fullWidth href="/admin">
           Admin Controls
         </Button>
