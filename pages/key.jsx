ad2d802b0a38f8383743c37710a21082b283f346
James Mitofsky <JamesMitofsky@gmail.com>
2022-09-08 18:21:17 +0200

    Bare bones working prototype

--- pages/key.jsx
+++ pages/key.jsx
@@ -18,10 +18,7 @@ import setState from "../functions/setState";
 
 export default function Key() {
   const [codeData, setCodeData] = useState("");
-  const [param, setParam] = useState({
-    serverCall: "",
-    userInput: "",
-  });
+  const [param, setParam] = useState("");
   const [states, setStates] = useState({
     loading: true,
     noCode: false,
@@ -33,39 +30,39 @@ export default function Key() {
     // don't continue evaluating function until certain there's no URL param inbound
     if (!isReady) return;
 
-    // throw param error in two cases:
-    // 1. no param at all
-    // 2. param does not exist (handle this in async func)
-    if (!param.userInput) {
+    // check if there's any key in the URL for a param
+    // if (query === "value") {
+    //   console.log("no key found");
+    //   return;
+    // }
+
+    // does correct key exist in URL?
+    const codeKey = Object.keys(query).find((key) => key === "value");
+    if (codeKey !== "value" || !query.value) {
       setState(setStates, "noCode");
+      console.log("Incorrect URL param key was provided");
       return;
     }
 
-    console.log("checking url");
-    checkURL();
+    console.log("ran");
 
     databaseSearch();
     // run the effect on either condition
     // 1. the first time once params are ready from React
     // 2. the input state has changed
-  }, [isReady, param.serverCall]);
-
-  // get url param for key
-  function checkURL() {
-    query["value"] &&
-      setParam((prev) => ({ ...prev, serverCall: query["value"] }));
-  }
+  }, [isReady]);
 
   async function databaseSearch() {
     // fetch object from firestore using the key
-    const docRef = doc(db, "globalKeys", param.serverCall);
+    const docRef = doc(db, "globalKeys", query["value"]);
     const res = await getDoc(docRef);
-    if (!res.exists()) return;
+    if (!res.exists()) {
+      setState(setStates, "noCode");
+      return;
+    }
 
+    // TODO: handle error: weekday won't exist on weekends
     const arrayOfWeekdays = res.data()?.weekdays;
-
-    // end function if no weekday match can be found -- this could mean there wasn't even a matching key
-    if (!arrayOfWeekdays) setNeedInput(true);
 
     const qrCodeValue = getCodeFromWeekdays(arrayOfWeekdays);
     setCodeData(qrCodeValue);
@@ -93,18 +90,18 @@ export default function Key() {
               fullWidth
               label="Enter your key name"
               value={param.userInput}
-              onChange={(e) =>
-                setParam((prev) => ({ ...prev, userInput: e.target.value }))
-              }
+              onChange={(e) => setParam(e.target.value)}
             />
-            <Button onClick={inputSubmitted}>Search</Button>
+            <Button fullWidth onClick={inputSubmitted}>
+              Search
+            </Button>
           </>
         )}
 
-        {states.loading && codeData && (
+        {states.showCode && (
           <>
             <CodeOfDay value={codeData.code} />
-            {!codeData && <NoCodeToday />}
+            {/* {!codeData && <NoCodeToday />} */}
             <WelcomeText />
           </>
         )}
