d342a944222a226b1579164ff4b21ba89f7a82c6
James Mitofsky <JamesMitofsky@gmail.com>
2022-09-10 23:58:35 +0200

    Move user auth logic into a try catch statement & increase fidelity of user data being returned to Next

--- functions/isUserAuthorized.js
+++ functions/isUserAuthorized.js
@@ -3,20 +3,22 @@ import { collection, query, where, getDocs } from "firebase/firestore";
 // local db config
 import { db } from "../services/firebase";
 
-export default async function isUserAuthorized(uidInquiry) {
+export default async function isUserAuthorized(uidInquiry, userObj) {
   if (!uidInquiry) return false;
-  const possibleRoles = ["admin", "resident"];
 
   const q = query(collection(db, "userRoles"), where("uid", "==", uidInquiry));
 
-  const querySnapshot = await getDocs(q);
-  const response = querySnapshot.docs[0].data();
+  try {
+    const querySnapshot = await getDocs(q);
+    const response = querySnapshot.docs[0].data();
 
-  // // iterate through all possible user roles
-  // const userRoles = possibleRoles.map((role) => {
-  //   // check all existing roles from the server object
-  //   const roleStatus = response.role.find((serverRole) => serverRole === role);
-  //   return roleStatus;
-  // });
-  return response;
+    const completeUser = {
+      ...response,
+      displayName: userObj.displayName,
+      photoURL: userObj.photoURL,
+    };
+    return response ? completeUser : false;
+  } catch {
+    console.warn("Not authorized to request user auth status");
+  }
 }
