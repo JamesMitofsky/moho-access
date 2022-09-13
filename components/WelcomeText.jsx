61a56ce446f3a35b651fc573cdc5b22fb96af548
James Mitofsky <JamesMitofsky@gmail.com>
2022-09-13 17:16:35 +0200

    Correct the font face on welcome text

--- components/WelcomeText.jsx
+++ components/WelcomeText.jsx
@@ -4,8 +4,11 @@ import { Typography } from "@mui/material";
 export default function WelcomeText() {
   return (
     <>
-      <Typography sx={{ textAlign: "center", fontWeight: 300 }} variant="h1">
-        BIENVENUE A<MohoSVG color={"#f16760"} height={58} />
+      <Typography
+        sx={{ textAlign: "center", fontFamily: "'Antonio'" }}
+        variant="h1"
+      >
+        BIENVENUE A<MohoSVG color={"#f16760"} height={65} />
         LOGIN
       </Typography>
     </>
